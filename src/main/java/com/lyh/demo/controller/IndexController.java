4c55fe6551009a18cc7a0ee5c7045da6a64d316b
shoushen <shoushen@yzyw-inc.com>
2019-01-20 16:18:27 +0800

    增加获取注册中心的实例信息

--- src/main/java/com/lyh/demo/controller/IndexController.java
+++ src/main/java/com/lyh/demo/controller/IndexController.java
@@ -1,7 +1,11 @@
 package com.lyh.demo.controller;
 
+import java.util.List;
 import java.util.Random;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.core.env.Environment;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,6 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
   @Autowired
   private RedisTemplate<String,Object> redisTemplate;
+  @Autowired
+  private DiscoveryClient discoveryClient;
+  @Autowired
+  private Environment environment;
   @ResponseBody
   @RequestMapping({"/","/index"})
   public Object index(){
@@ -24,5 +32,22 @@ public class IndexController {
     return redisTemplate.opsForValue().get("kevin");
   }
 
+  /**
+   * 获取服务名称
+   * @return
+   */
+  @RequestMapping(value="/getServices")
+  public List<String> getServices(){
+    return discoveryClient.getServices();
+  }
 
+  /**
+   * 获取注册实例列表
+   * @return
+   */
+  @RequestMapping(value="/getInstance")
+  public List<ServiceInstance> getInstance(){
+    String sysName= environment.getProperty("spring.application.name");
+    return discoveryClient.getInstances(sysName);
+  }
 }
